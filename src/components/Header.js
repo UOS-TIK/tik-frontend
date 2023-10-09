4eb0be9ac9ffcaca25f475e1efcd078c8de026dc
alicehrk <alicehrk23@gmail.com>
2023-10-10 00:26:53 +0900

    main 화면 구성

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,49 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Header = () => {
   return (
-    <div>
-      header
-    </div>
+    <HeaderContainer>
+      <div style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+        <Link to="/">
+          <img src="images/ic_logo.svg" alt="logo_icon"/>
+        </Link>
+        <MenuContainer>
+          <MenuItem>면접 생성</MenuItem>
+          <MenuItem>이력 조회</MenuItem>
+          <MenuItem>회원 정보</MenuItem>
+        </MenuContainer>
+      </div>
+      <LogoutButton>로그아웃</LogoutButton>
+    </HeaderContainer>
   );
 };
 
 export default Header;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 80px;
+  color: white;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+  color: #606060;
+  font-size: 16px;
+`;
+
+const MenuContainer = styled.div`
+  display: flex;
+  gap: 72px;
+`;
+
+const MenuItem = styled.div`
+  cursor: pointer;
+`;
+
+const LogoutButton = styled.div`
+  cursor: pointer;
+`;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,30 @@
+import styled from "styled-components";
+import Header from "../components/Header";
+
 const Main = () => {
   return (
-    <div>main</div>
+    <div style={{display: "flex", flexDirection: "column"}}>
+      <Header />
+      <MainBanner>
+        <div style={{display: "flex", flexDirection: "column", gap: "16px", marginTop: "40px"}}>
+          <div style={{width: "fit-content", color: "#FFFFFFCC", backgroundColor: "#3D4371", padding: "6px 8px", borderRadius: "7px", fontSize: "15px", fontWeight: "700"}}>실제 면접과 유사</div>
+          <div style={{color: "#FFFFFF", display: "flex", flexDirection: "column", gap: "8px"}}>
+            <div style={{fontSize: "25px", fontWeight: "800"}}>AI 면접관의 목소리를 들어보세요!</div>
+            <div style={{fontSize: "17px"}}>면접관과 목소리로 대화하며 면접 실전 감각을 기를 수 있습니다.</div>
+          </div>
+        </div>
+        <img src="images/banner_img.svg" alt="banner_img"/>
+      </MainBanner>
+    </div>
   );
 };
 
 export default Main;
+
+const MainBanner = styled.div`
+  display: flex;
+  justify-content: space-around;
+  background: #8D99F3;
+  width: 100%;
+  height: 300px;
+`;
