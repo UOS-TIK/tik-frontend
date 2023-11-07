c3a95196da2f5c925a46bb796a94339209609d66
graS2e <77924292+graS2e@users.noreply.github.com>
2023-11-07 23:55:15 +0900

    feat: LoginInput 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import GenerateInterview from './pages/GenerateInterview';
-import Main from './pages/Main';
-import Test from './pages/Test/Test';
-import ReactSpeechKit from './pages/Test/ReactSpeechKit';
-import ReactSpeechRecognition from './pages/Test/ReactSpeechRecognition';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GenerateInterview from "./pages/GenerateInterview";
+import Main from "./pages/Main";
+import Test from "./pages/Test/Test";
+import ReactSpeechKit from "./pages/Test/ReactSpeechKit";
+import ReactSpeechRecognition from "./pages/Test/ReactSpeechRecognition";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/generate-interview' element={<GenerateInterview />} />
-        <Route path='/test' element={<Test />} />
-        <Route path='/test/1' element={<ReactSpeechKit />} />
-        <Route path='/test/2' element={<ReactSpeechRecognition />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/generate-interview" element={<GenerateInterview />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/test/1" element={<ReactSpeechKit />} />
+        <Route path="/test/2" element={<ReactSpeechRecognition />} />
       </Routes>
     </BrowserRouter>
   );
