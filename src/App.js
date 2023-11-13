6b4fcf130ff375a8199f636affdc547914892f90
graS2e <77924292+graS2e@users.noreply.github.com>
2023-11-13 14:34:47 +0900

    del: Input test code 삭제

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Test from "./pages/Test/Test";
 import ReactSpeechKit from "./pages/Test/ReactSpeechKit";
 import ReactSpeechRecognition from "./pages/Test/ReactSpeechRecognition";
 import Login from "./pages/Login";
-import InputTest from "./pages/InputTest";
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
         <Route path="/test/1" element={<ReactSpeechKit />} />
         <Route path="/test/2" element={<ReactSpeechRecognition />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/input-test" element={<InputTest />} />
       </Routes>
     </BrowserRouter>
   );
