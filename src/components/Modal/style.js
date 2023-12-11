9884acbd7ae8107aa8c9523c6fc2ac8748d1aa50
graS2e <77924292+graS2e@users.noreply.github.com>
2023-12-12 00:03:07 +0900

    Update style.js

--- src/components/Modal/style.js
+++ src/components/Modal/style.js
@@ -5,7 +5,7 @@ export const ModalWrapper = styled.div`
   z-index: 1000;
   transition: all 0.3s ease-in-out;
   position: fixed;
-  top: 50%;
+  top: 42%;
   left: 50%;
   transform: ${(props) =>
     props.isModalOn ? "translate(-50%, -50%)" : "translate(-50%, -40%)"};
